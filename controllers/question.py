6d5903a6b012c977fb6c8a6caf12638236e33910
newglobalstrategy <newglobalstrategy@gmail.com>
2020-11-01 23:38:03 +0000

    simple forms now working and back to allowing answer text but only 2 answers are allowed

--- controllers/question.py
+++ controllers/question.py
@@ -13,44 +13,40 @@ from py4web.utils.form import Form
 from yatl.helpers import A
 from ..common import db, session, T, cache, auth, logger, authenticated, unauthenticated
 
+
 @action("new_question", method=['GET', 'POST'])
 @action.uses('new_question.html', session, db)
 def new_question():
-    # This allows creation and editing of projects by their owner
-    # 'answer_group' removed as no other security functions for projects and events yet - not currently needed
-    #fields = ['proj_name', 'description', 'proj_url', 'startdate', 'enddate', 'proj_shared']
-    #projid = request.args(0, default=None)
-    #if projid is not None:
-    #    record = db.project(projid)
-    #    if record.proj_owner != auth.user_id:
-    #        session.flash = 'Not Authorised - projects can only be edited by their owners'
-    #        redirect(URL('new_project'))
-    #    form = SQLFORM(db.project, record, fields=fields)
-    #else:
-    qtype = request.args(0, default='quest')
-    questid = request.args(1, cast=int, default=0)
+    # TODO find out how request.args works with bottle prob different
+    # qtype = request.args(0, default='quest')
+    # questid = request.args(1, cast=int, default=0)
+
+    form = Form([db.question.questiontext,
+                 db.question.factopinion,
+                 db.question.answertext,
+                 db.question.answer1,
+                 db.question.answer2])
+
+    return dict(form=form)
 
 
+@action("new_action", method=['GET', 'POST'])
+@action.uses('new_question.html', session, db)
+def new_action():
+    # TODO need to make label dynamic
+    form = Form([db.question.questiontext,
+                 db.question.responsible,
+                 db.question.startdate,
+                 db.question.enddate])
 
-    form = Form(db.question)
+    return dict(form=form)
 
-    #if form.validate():
-    #    if projid is not None:
-    #        if form.deleted:
-    #            db(db.project.id == projid).delete()
-    #            response.flash = 'Project deleted'
-    #            redirect(URL('default', 'index'))
-    #        else:
-    #            record.update_record(**dict(form.vars))
-    #            response.flash = 'Project updated'
-    #            redirect(URL('default', 'index'))
-    #    else:
-    #        form.vars.id = db.project.insert(**dict(form.vars))
-    #        session.flash = 'Project Created'
-    #        redirect(URL('accept_project', args=[form.vars.id, auth.user_id]))
-    #elif form.errors:
-    #    response.flash = 'form has errors'
-    #else:
-    #    response.flash = 'please fill out the form'
+
+@action("new_issue", method=['GET', 'POST'])
+@action.uses('new_question.html', session, db)
+def new_issue():
+    # TODO need to make label dynamic
+    # Sticking with approach that you don't get to grade your own issues for urgency and importance at creation
+    form = Form([db.question.questiontext])
 
     return dict(form=form)
