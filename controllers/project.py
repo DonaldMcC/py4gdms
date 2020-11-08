e4f2db3fd917de9015603522415f73e4e8c638d9
newglobalstrategy <newglobalstrategy@gmail.com>
2020-11-08 10:37:25 +0000

    add project grid - mainly working - not tried search yet

--- controllers/project.py
+++ controllers/project.py
@@ -1,8 +1,10 @@
 
 from py4web import action, request, abort, redirect, URL
-from py4web.utils.form import Form
+from py4web.utils.form import Form, FormStyleBulma
 from yatl.helpers import A
 from ..common import db, session, T, cache, auth, logger, authenticated, unauthenticated
+#from .libs.utils import GridSearch
+from py4web.utils.grid import Grid, GridClassStyleBulma
 
 
 @action("new_project", method=['GET', 'POST'])
@@ -41,3 +43,39 @@ def new_project():
     #    response.flash = 'please fill out the form'
 
     return dict(form=form)
+
+
+@action('projectgrid', method=['POST', 'GET'])
+@action('projectgrid/<path:path>', method=['POST', 'GET'])
+@action.uses(session, db, auth.user, 'projectgrid.html')
+def projectgrid(path=None):
+    GRID_DEFAULTS = dict(rows_per_page=15,
+                         include_action_button_text=True,
+                         search_button_text='Filter',
+                         formstyle=FormStyleBulma,
+                         grid_class_style=GridClassStyleBulma)
+
+    fields = [db.project.proj_name, db.project.proj_status, db.project.startdate, db.project.enddate,
+              db.project.proj_owner]
+
+    orderby = [db.project.proj_name]
+
+    queries = [(db.project.id > 0)]
+
+    search_queries = [['Search by Name', lambda value: db.project.name == value]]
+
+    #search = GridSearch(search_queries, queries)
+
+    grid = Grid(path,
+                db.project,
+                fields=fields,
+                headings=['Name', 'Location', 'Project', 'Status', 'Starttime', 'EndTime', 'Description', 'Shared'],
+                orderby=orderby,
+                search_queries=search_queries,
+                create=True,
+                details=True,
+                editable=True,
+                deletable=True,
+                **GRID_DEFAULTS)
+
+    return dict(grid=grid)
