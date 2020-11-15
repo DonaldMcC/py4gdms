6bf86bc05fd4566129ba7589b6d06b2be7f650c2
newglobalstrategy <newglobalstrategy@gmail.com>
2020-11-15 20:24:54 +0000

    added files to start test setup

---
+++ fts/functional_tests.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# this is now using the HTLMLTestrunner approach - however cant get that working to file other than by piping
+# at the command line so approach for now is to get command line at main directory and run:
+# python .\fts\functional_tests.py >testddmm.html  to get the file and then open in a browser
+#
+import unittest
+import HTMLTestRunner
+import sys
+try:
+	from urllib2 import urlopen
+except ImportError:
+	from urllib.request import urlopen
+from selenium import webdriver
+import subprocess
+import sys
+import os.path
+
+sys.path.append('./fts/lib')
+sys.path.append('../../gluon')  # to use web2py modules
+
+# ROOT = 'http://localhost:8080/gdms'
+ROOT = 'http://localhost:8081/gdms'
+# ROOT = 'http://localhost:8081/gdms'
+# ROOT = 'https://www.netdecisionmaking.com/gdmssql'
+
+NUMCYCLES = 2  # Reduce this for quicker runs
+CACHETIME = 1
+STARTSERVER = False
+
+# may update these later but possibly just have 3 options for now
+USERS={'USER1':'Testuser1','PASSWORD1':'user1',
+        'USER2':'Testuser2','PASSWORD2':'user2','USER3':'Testuser3','PASSWORD3':'user3','USER4':'Testuser4','PASSWORD4':'user4',
+       'USER5':'Testuser5','PASSWORD5':'user5','USER6':'Testuser6','PASSWORD6':'user6','USER7':'Testuser7','PASSWORD7':'user7',
+       'USER8':'Testuser8','PASSWORD8':'user8','USER9':'Testuser9','PASSWORD9':'user9'}
+
+listusers = ['user2', 'user3', 'user4']
+questref =  'functest questref'
+votequest = 'tempvotetest'
+testconfig = ('standard')
+# testconfig = ('verify','local')
+
+
+class FunctionalTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        global STARTSERVER
+        if STARTSERVER:
+            self.web2py = start_web2py_server()
+        # self.browser = webdriver.Firefox()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--disable-extensions')
+
+        self.browser = webdriver.Chrome('d:\python37\chromedriver.exe',chrome_options=chrome_options)
+        self.browser.maximize_window()
+
+        #self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(10)
+
+    @classmethod    
+    def tearDownClass(self):
+        global STARTSERVER
+        self.browser.close()
+        if STARTSERVER:
+            self.web2py.kill()
+
+    def get_response_code(self, url):
+        """Returns the response code of the given url
+
+        url     the url to check for 
+        return  the response code of the given url
+        """
+
+        handler = urlopen(url, timeout=5)
+        return handler.getcode()
+
+
+def start_web2py_server():
+    # noreload ensures single process
+    #print (os.path.curdir)
+    return subprocess.Popen([
+            'python', '../../web2py.py', 'runserver', '-a "passwd"', '-p 8001'
+    ])
+
+def run_functional_tests(pattern=None):
+    print ('running tests')
+    if pattern is None:
+        tests = unittest.defaultTestLoader.discover('fts')
+    else:
+        pattern_with_globs = '*%s*' % (pattern,)
+        tests = unittest.defaultTestLoader.discover('fts', pattern=pattern_with_globs)
+
+    # neither of these actually write to file so just using >filename.html on the command line
+    # runner = unittest.TextTestRunner()
+    runner = HTMLTestRunner.HTMLTestRunner(verbosity=2)
+    runner.run(tests)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        run_functional_tests()
+    else:
+        run_functional_tests(pattern=sys.argv[1])
---
+++ fts/test_p0aregister.py
@@ -0,0 +1,62 @@
+from functional_tests import FunctionalTest, ROOT, USERS
+import time
+from ddt import ddt, data, unpack
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+# element = WebDriverWait(driver, 10).until(lambda driver : driver.find_element_by_id("createFolderCreateBtn"))
+@ddt
+class TestRegisterPage (FunctionalTest):
+    def setUp(self):     
+        self.url = ROOT + '/default/user/register'        
+        get_browser = self.browser.get(self.url)
+
+    # def test_can_view_register_page(self):
+    #    response_code = self.get_response_code(self.url)        
+    #    self.assertEqual(response_code, 200)    
+
+    # def test_has_right_title(self):
+    #    title = self.browser.title        
+    #    #self.assertEqual(u'Net Decision Making: Registration', title)
+    #    self.assertIn('Networked Decision Making', title)
+
+    @data((USERS['USER1'], USERS['PASSWORD1']))
+    @unpack
+    def test_put_values_in_regester_form(self, user, passwd):
+
+        # first_name = self.browser.find_element_by_name("first_name")
+        first_name = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_name("first_name"))
+        first_name.clear()
+        first_name.send_keys(user)
+        
+        last_name = self.browser.find_element_by_name("last_name")
+        last_name.clear()
+        last_name.send_keys(user)
+
+        mailstring = user+'@user.com'
+        email = self.browser.find_element_by_name("email")
+        email.clear()
+        email.send_keys(mailstring)
+
+        # username = self.browser.find_element_by_name("username")
+        # username.clear()
+        # username.send_keys(user)
+
+        password = self.browser.find_element_by_name("password")
+        password.clear()
+        password.send_keys(passwd)
+
+        verify_password = self.browser.find_element_by_name("password_two")
+        verify_password.clear()
+        verify_password.send_keys(passwd)
+
+        data_consent = self.browser.find_element_by_name("data_consent")
+        data_consent.click()
+
+        register_button = self.browser.find_element_by_css_selector("#submit_record__row input")
+
+        register_button.click()
+        time.sleep(3)
+        resultstring = 'Welcome ' + user
+        body = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_tag_name('body'))
+        self.assertIn(resultstring, body.text)
