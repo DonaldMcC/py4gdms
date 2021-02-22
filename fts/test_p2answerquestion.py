26ac3f22d3d0a95718c0ce119b6201f693f4bb8e
newglobalstrategy <newglobalstrategy@gmail.com>
2021-02-22 23:23:05 +0000

    fix defaults for questions

--- fts/test_p2answerquestion.py
+++ fts/test_p2answerquestion.py
@@ -15,8 +15,7 @@ from selenium.webdriver.common.by import By
 class AnswerQuestion (FunctionalTest):
 
     def setUp(self):   
-        self.url = ROOT + '/default/user/login'        
-        get_browser = self.browser.get(self.url)
+        pass
 
     @data((USERS['USER2'], USERS['PASSWORD2'], 'in progress'), (USERS['USER3'], USERS['PASSWORD3'], 'in progress'),
           (USERS['USER4'], USERS['PASSWORD4'], 'Well done'))
@@ -25,6 +24,7 @@ class AnswerQuestion (FunctionalTest):
         self.url = ROOT + '/auth/login'
         get_browser = self.browser.get(self.url)
         time.sleep(2)
+
 
         email = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_id("none_username"))
         email.send_keys(user)
@@ -35,35 +35,21 @@ class AnswerQuestion (FunctionalTest):
         submit_button.click()
         time.sleep(1)
 
-        self.url = ROOT + '/answer/get_question/quest'
+        self.url = ROOT + '/'
         get_browser = self.browser.get(self.url)
         time.sleep(1)
         #self.browser.find_element_by_xpath("(//input[@name='ans'])[2]").click()
 
-        wait = WebDriverWait(self.browser, 12)
-        element = wait.until(EC.element_to_be_clickable((By.XPATH, "(//input[@name='ans'])[2]")))
-        element.click()
+        #answer issue
+        self.browser.find_element(By.CSS_SELECTOR, "td:nth-child(3) > .is-success").click()
 
-        urgency = self.browser.find_element_by_id("userquestion_urgency")
-        urgency.send_keys("7")
-        importance = self.browser.find_element_by_id("userquestion_importance")
-        importance.send_keys("8")
-        self.browser.find_element_by_id("userquestion_changecat").click()
-    
-        category = self.browser.find_element_by_id("userquestion_category")
-        category.send_keys("Strategy")
-        self.browser.find_element_by_id("userquestion_changescope").click()
-        self.browser.find_element_by_id("userquestion_answerreason").send_keys("the right answer selenium testing")
-
-        submit_button = self.browser.find_element_by_css_selector("#submit_record__row input")
-        submit_button.click()
         time.sleep(1)
 
         #body = self.browser.find_element_by_tag_name('body')
         body = WebDriverWait(self, 10).until(lambda self : self.browser.find_element_by_tag_name('body'))
         self.assertIn(result, body.text)
 
-        self.url = ROOT + '/default/user/logout'
-        get_browser=self.browser.get(self.url)
-
+        self.url = ROOT + '/auth/logout'
+        get_browser = self.browser.get(self.url)
+        time.sleep(1)
         
