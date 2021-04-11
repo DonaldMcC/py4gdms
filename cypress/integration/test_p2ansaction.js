4695269502e41d1b8488984c05d6e1a32af7c204
Donald McClymont <Donaldm2020@gmail.com>
2021-04-11 09:53:11 +0100

    start on answering questions and actions

---
+++ cypress/integration/test_p2ansaction.js
@@ -0,0 +1,41 @@
+/*npx cypress open - is the command to get started on these for now  */
+
+describe('Answer Actions',  () => {
+    // we can use these values to log ind
+    const username = 'user2'
+    const password = 'Testuser2'
+
+
+    const operations = [
+        {
+            url: '/index/actions',
+            answer: 'Approve'
+        },
+        {
+            url: '/index/actions',
+            answer: 'Approve'
+        },
+        {
+            url: '/index/actions',
+            answer: 'Approve'
+        }
+    ]
+
+    // dynamically create a single test for each operation in the list
+    operations.forEach((action) => {
+
+                it('can visit /users', function () {
+                    // or another protected page
+                    cy.visit('/auth/login?next=../index')
+                    cy.get('#signin').type(username)
+                    cy.get('#signpass').type(password)
+                    cy.get('#login').click()
+
+                    cy.visit(action.url)
+                   cy.get("td:nth-child(2) > .is-success").click()
+                    cy.get('body').should('contain', 'Answer recorded')
+                })
+    })
+})
+
+
