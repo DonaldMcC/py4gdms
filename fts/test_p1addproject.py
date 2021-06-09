39dfc39d256f998065ec0ca2fcdb4eaf0cf0daa9
Donald McClymont <Donaldm2020@gmail.com>
2021-06-09 22:01:19 +0100

    start more testing

--- fts/test_p1addproject.py
+++ fts/test_p1addproject.py
@@ -25,7 +25,7 @@ class AddBasicAction (FunctionalTest):
         time.sleep(1)
 
 
-    @data(('/new_question/0/action', 'Lets get this done'), ('/new_question/0/issue', 'The world is under-achieving'))
+    @data(('/new_project', 'Phase 1 test project'), ('/new_project', 'Phase 1 test project2'))
     @unpack
     def test_question(self, urltxt, itemtext):
         self.url = ROOT + urltxt
