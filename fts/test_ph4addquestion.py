b6b25b16ba9827d59ae3d2ba570791fba6c406c8
Donald McClymont <Donaldm2020@gmail.com>
2021-08-30 19:07:26 +0100

    make project grid responsive

---
+++ fts/test_ph4addquestion.py
@@ -0,0 +1,59 @@
+# These tests are all based on the tutorial at http://killer-web-development.com/
+# THis test will look at taking question to resolved and then challenging - so this just creates ph4 question
+
+
+from functional_tests import FunctionalTest, ROOT, USERS, CACHETIME, questidlist, questiddict
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class AddBasicQuestion (FunctionalTest):
+
+    def setUp(self):
+        self.url = ROOT + '/auth/login'
+        self.browser.get(self.url)
+        time.sleep(2)
+        email = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_id("signin"))
+        email.send_keys(USERS['USER2'])
+        password = self.browser.find_element_by_id("signpass")
+        password.send_keys(USERS['PASSWORD2'])
+        # submit_button = self.browser.find_element_by_css_selector("input[type=submit]")
+        submit_button = self.browser.find_element_by_id("login")
+        submit_button.click()
+        time.sleep(1)
+
+    def test_question(self):
+        self.url = ROOT + '/new_question/None/quest'
+        self.browser.get(self.url)
+        time.sleep(2)  # still getting blank category for some reason but not if loaded manually
+        # questiontext = self.browser.find_element_by_name('questiontext')
+        itemtext = "Ph4 question to resolve and unresolve"
+        questiontext = WebDriverWait(self, 10).until(lambda self:
+                                                     self.browser.find_element_by_id('question_questiontext'))
+        questiontext.send_keys(itemtext)
+
+        ans1 = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_id("question_answer1"))
+        ans1.send_keys("yes")
+
+        ans2 = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_id("question_answer2"))
+        ans2.send_keys("no")
+
+        submit_button = self.browser.find_element_by_css_selector("input[type=submit]")
+        submit_button.click()
+        time.sleep(1)
+  
+        # Lookof for body in questiongrid
+        body = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_tag_name('body'))
+        self.assertIn(itemtext, body.text)
+
+        alertarea = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_id('alertarea'))
+        self.assertIn("ID", alertarea.text)
+        recordpos = alertarea.text.find('RecordID')
+        if recordpos > 0:
+            recordstr = alertarea.text[recordpos+9:]
+            # print('recstr:'+recordstr)
+            recordval=int(recordstr) if recordstr.isnumeric() else 0
+            # print(recordval)
+            questidlist.append(recordval)
+            questiddict['ph4quest'] = recordval
+        time.sleep(CACHETIME)
---
+++ fts/test_ph4answerquestion.py
@@ -0,0 +1,53 @@
+# These tests are all based on the tutorial at http://killer-web-development.com/
+# if registration is successful this may work but lets
+# try and get user logged in first
+
+
+from functional_tests import FunctionalTest, ROOT, USERS, questiddict
+import time
+from ddt import ddt, data, unpack
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+
+@ddt
+class AnswerQuestion (FunctionalTest):
+
+    def setUp(self):   
+        pass
+
+    @data((USERS['USER2'], USERS['PASSWORD2'], 'Answer recorded'),
+          (USERS['USER3'], USERS['PASSWORD3'], 'Answer recorded'),
+          (USERS['USER4'], USERS['PASSWORD4'], 'Resolved'))
+    @unpack
+    def test_answer(self, user, passwd, result):
+        self.url = ROOT + '/auth/login'
+        self.browser.get(self.url)
+        time.sleep(2)
+        qid = questiddict.get('p2quest')
+
+        email = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_id("signin"))
+        email.send_keys(user)
+        password = self.browser.find_element_by_id("signpass")
+        password.send_keys(passwd)
+        # submit_button = self.browser.find_element_by_css_selector("input[type=submit]")
+        submit_button = self.browser.find_element_by_id("login")
+        submit_button.click()
+        time.sleep(1)
+
+        self.url = ROOT + '/index/questions/'+str(qid)
+        self.browser.get(self.url)
+        time.sleep(1)
+        # self.browser.find_element_by_xpath("(//input[@name='ans'])[2]").click()
+
+        self.browser.find_element(By.CSS_SELECTOR, "td:nth-child(5) > .is-success").click()
+
+        time.sleep(1)
+
+        # body = self.browser.find_element_by_tag_name('body')
+        body = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_tag_name('body'))
+        self.assertIn(result, body.text)
+
+        self.url = ROOT + '/auth/logout'
+        self.browser.get(self.url)
+        time.sleep(1)
