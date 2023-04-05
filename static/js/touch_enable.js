681cb3863a716e4afc3c44b8eb017907630f4f32
Donald McClymont <Donaldm2020@gmail.com>
2023-04-05 22:34:23 +0100

    add touch_enable

---
+++ static/js/touch_enable.js
@@ -0,0 +1,9 @@
+//enable touch for dropdown
+document.addEventListener("touchstart", function() {}, true);
+$(document).ready(function() {
+
+    /*  $('.hover').on('touchstart touchend', function(e){
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });*/
+});
