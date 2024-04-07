e1699ad83768d19f2a2bfad1b36567c43bf9854c
Donald McClymont <Donaldm2020@gmail.com>
2024-04-07 09:14:19 +0100

    further tidy of graphd3v4 no planned changes

--- static/js/graphd3v4.js
+++ static/js/graphd3v4.js
@@ -209,17 +209,13 @@ function redrawnodes() {
 
     var link = svg.select("#links").selectAll('.link')
             .data(edges)
-            .classed(consts.selectedClass, function(event, d){
-            return d === state.selectedEdge;
-            })
+            .classed(consts.selectedClass, function(event, d){return d === state.selectedEdge;})
             .enter()
             .append("path")
             .attr("class", "link")
             .on("click", function(event, d) {linkclick(event, d);})
             .on("touchstart", function(event, d) {linkclick(event, d);})
-            .attr("d", function(d){
-        return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;
-      })
+            .attr("d", function(d){return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;})
         .classed("link", true)
         .attr("stroke", "purple")
         .style("stroke-width", function(event, d){return d.linethickness})
@@ -248,42 +244,37 @@ function redrawnodes() {
         .style("fill", function(d){return d.fillclr})
         .style("stroke", function(d){return d.scolour})
         .style("stroke-dasharray", function(d){if (d.status=='Draft') {return ("8,8")} else {return ("1000,1")}}) // make the stroke dashed
-        .style("stroke-width", function(d){return d.swidth})
-         /*.attr('height', 25)*/
-        ;
+        .style("stroke-width", function(d){return d.swidth});
 
     node.each(function(d) {
         var numquests = 0;
         if (d.subquests != null)
         { var numquests = d.subquests.length};
         wrapText(d3.select(this), d.title,  numquests, d.qtype, d.perccomplete);
-        drawtooltip(d);
-    });
+        drawtooltip(d);});
 
-    //V E L A D M view, edit, link, add, delete, demote
-    //So getting real problems with click events not triggering instead only the
-    //drag event was firing - think we overcome this with a justDragged variable 
-    //and calling fromdrag for now
-function rectclick(event) {
+        //V E L A D M view, edit, link, add, delete, demote
+        //So getting real problems with click events not triggering instead only the
+        //drag event was firing - think we overcome this with a justDragged variable
+        //and calling fromdrag for now
+    function rectclick(event) {
         // lets replace this with launching question url in new tab
         //console.log("you clicked rectd ", d.serverid);
         //think this will become an ajax load presently
         location.href = baselowerUrl+'/1/'+d.serverid+'/';
-        event.stopPropagation();
-    };
+        event.stopPropagation();};
 
     function urlclick(event, d) {
         // lets replace this with launching question url in new tab
         //console.log("you clicked rectd ", d.serverid);
         //think this will become an ajax load presently
-        console.log('you clicked url')
-        console.log(d)
+        //console.log('you clicked url')
+        //console.log(d)
         event.stopPropagation();
         if (d.question_url >'') {
             window.open(d.question_url, '_blank').focus();
             event.stopPropagation();
-        };
-    };
+        };};
 
     function nodeclick(event, d) {
         //alert("you clicked node", d.serverid);
@@ -408,8 +399,7 @@ function rectclick(event) {
             case 'D':
                 //Edit - this should load the URL and
                 //console.log("this will call delete link");
-                deleteLink(edges[edges.indexOf(d)].source.serverid.toString(),
-                    edges[edges.indexOf(d)].target.serverid.toString(), 'delete');
+                deleteLink(edges[edges.indexOf(d)].source.serverid.toString(), edges[edges.indexOf(d)].target.serverid.toString(), 'delete');
                 //console.log(edges.length);
                 //console.log(d.source,d.target);
                 var index = edges.indexOf(d);
@@ -425,9 +415,6 @@ function rectclick(event) {
         event.stopPropagation();
     }
 
-    //don't think below are needed now - just define as part of drawing
-    //link.on("click", linkclick);
-    //node.on("click", function(event, d) {nodeclick(event, d);});
 
     svg.on("click", backclick);
 
@@ -470,21 +457,14 @@ function rectclick(event) {
         }
 
         fieldformat += "<TR><TD><B>" + qtype + "</B></TD><TD colspan='3'>" + notes + "</TD></TR>";
-
         if (qtype == 'Action') {
             fieldformat += "<TR><TD><B>Due Date</B></TD><TD>" + d.duedate + "</TD><TD><B>" + " Responsible:" + "</B></TD><TD>" + d.responsible + "</TD></TR>";
         };
 
         fieldformat += "<TR><TD><B>Status</B></TD><TD>"+ d.status+"</TD><TD><B>"+" Priority:"+"</B></TD><TD>"+ d.priority+"</TD></TR>";
-
         if (d.question_url > '') {
                 fieldformat += "<TR><TD><B>Link</B></TD><TD colspan='3'>" + d.question_url + "</TD></TR>";
             };
-
-           //             .style("left", 10 + (event.pageX + 10) + "px")// or just (d.x + 50 + "px") (event.pageX)
-           //     .style("top", (event.pageY - 20) + "px")// or ...(event.pageY - 20)
-
-        //function(d) { return x(d) + "px"; })
 
         fieldformat += "</TABLE>";
         // Define 'div' for tooltips
@@ -500,32 +480,19 @@ function rectclick(event) {
                 .style("width", 50 + "px")
                 .transition()
                 .duration(800)
-                .style("opacity", 0);
-    };
-
+                .style("opacity", 0);};
 
 //need to actually figure out what goes in the tooltip 
     node.on("mouseover", function(event, d) {
         newid = "#tooltip" + this.id.substring(6)
-        var g = d3.select(newid).style("opacity", 1);
-    });
+        var g = d3.select(newid).style("opacity", 1);});
 
     node.on("mouseout", function(event, d) {
         newid = "#tooltip" + this.id.substring(6)
-        var g = d3.select(newid).style("opacity", 0);
-        //d3.select("#graph").select('div.tooltip').remove();
-    });
+        var g = d3.select(newid).style("opacity", 0);});
 
-        function dragnodestarted(event, d) {
-            //d.fx = d.x;
-            //d.fy = d.y;
-        }
 
-        function dragnode(event, d) {
-            //console.log('dragging');
-            //console.log(d.id);
-            //console.log(d.x);
-
+    function dragnode(event, d) {
             switch (inputmode) {
                 case 'E':
                     //d.fx = event.x;
@@ -546,7 +513,6 @@ function rectclick(event) {
             //console.log('drag ended');
             //d.fx = null;
             //d.fy = null;
-
             lastserverid = d.serverid.toString();
             lastxpos = Math.floor(rescale(d.x,1000,width));
             lastypos = Math.floor(rescale(d.y,1000,height));
@@ -555,101 +521,88 @@ function rectclick(event) {
             graphvars.justDragged = true;
         }
 
-if (d32py.redraw == true) {
-            redrawGraph()
-}
+    if (d32py.redraw == true) {redrawGraph()}
 
-// ** Update data section (Called from the onclick)
-function redrawlines() {
+    // ** Update data section (Called from the onclick)
+    function redrawlines() {
     svg.selectAll('.link')
         .data(edges)
-        .attr("d", function (d) {
-            return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;
-        });
-}
+        .attr("d", function (d) {return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;});
+    };
 
-//            .force("charge", d3.forceManyBody().strength(-50000))
 
-function redrawGraph() {
-    var attractForce = d3.forceManyBody().strength(2000).distanceMax(100000)
+    function redrawGraph() {
+        var attractForce = d3.forceManyBody().strength(2000).distanceMax(100000)
                      .distanceMin(1000);
-    var repelForce = d3.forceManyBody().strength(-3000).distanceMax(800)
+        var repelForce = d3.forceManyBody().strength(-3000).distanceMax(800)
                    .distanceMin(1);
-
-    var simulation = d3.forceSimulation()
-        .force("attractForce",attractForce)
-        .force("link", d3.forceLink().id(function(event, d) { return d.id; }))
-        .force("repelForce",repelForce)
+        var simulation = d3.forceSimulation()
+            .force("attractForce",attractForce)
+            .force("link", d3.forceLink().id(function(event, d) { return d.id; }))
+            .force("repelForce",repelForce)
             .force("center", d3.forceCenter(width / 2, height / 2))
-             .force("y", d3.forceY(height / 2).strength(0.07))
+            .force("y", d3.forceY(height / 2).strength(0.07))
             .force("x", d3.forceX(width / 2).strength(0.05));
 
-    function strength() { return 0.1; }
-    function distance() { return 200; }
+        function strength() { return 0.1; }
+        function distance() { return 200; }
 
-   simulation
-        .nodes(nodes)
-        .on("tick", tick)
-        .on('end', function() {
-        // layout is done
-        writetoserver();
-        });
+        simulation
+            .nodes(nodes)
+            .on("tick", tick)
+            .on('end', function() {
+            // layout is done
+            writetoserver();
+            });
 
-    simulation.force("link")
-        .links(edges)
-        .distance(distance)
-        .strength(strength);
+        simulation.force("link")
+            .links(edges)
+            .distance(distance)
+            .strength(strength);
 
-
-    function tick() {
-        node.attr("transform", function (d) {
-            d.x = Math.max(consts.nodeRadius, Math.min(width - consts.nodeRadius, d.x));
+        function tick() {
+            node.attr("transform", function (d) {d.x = Math.max(consts.nodeRadius, Math.min(width - consts.nodeRadius, d.x));
             d.y = Math.max(consts.nodeRadius, Math.min(height - consts.nodeRadius, d.y));
             return "translate(" + d.x + "," + d.y + ")";
         });
 
         redrawlines();
-
     }
-    //console.log('forcenodes', nodes)
-
 }
 
-function writetoserver() {
-    if (d32py.vieweventmap == true && d32py.editable == true) {
-        // if owner and eventmapiterate through nodes and call function to write new positions to server
-        nodes.forEach(function (e) {
+    function writetoserver() {
+        if (d32py.vieweventmap == true && d32py.editable == true) {
+            // if owner and eventmapiterate through nodes and call function to write new positions to server
+            nodes.forEach(function (e) {
             //console.log(e.serverid.toString() + ':' + Math.floor(e.x).toString() + ':' + Math.floor(rescale(e.x, 1000, width)).toString());
             moveElement(e.serverid.toString(), Math.floor(rescale(e.x, 1000, width)).toString(),
-                Math.floor(rescale(e.y, 1000, height)).toString());
-        })
+                Math.floor(rescale(e.y, 1000, height)).toString());})
+        }
     }
-}
 
-function clearText(gEl) {
-     var el = gEl.selectAll("text");
+    function clearText(gEl) {
+        var el = gEl.selectAll("text");
         el.remove('tspan');
- }
+    }
 
+    // think these may become methods from naming setup
+    function wrapText(gEl, title, numsubs, qtype, perccomplete) {
+        var i = 0;
+        var line = 0;
+        var words = title.split(" ");
 
-// think these may become methods from naming setup
-function wrapText(gEl, title, numsubs, qtype, perccomplete) {
-     var i = 0;
-     var line = 0;
-     var words = title.split(" ");
-
-    var rc = gEl.append("rect")
+        var rc = gEl.append("rect")
              .attr("x", 45)
              .attr("y", 45)
              .attr("stroke", "blue")
              .attr("width", 20)
-              .attr("height", 20)
+             .attr("height", 20)
              .on("click", function(event, d) {urlclick(event, d)});
-     var rct = gEl.append("text")
+        var rct = gEl.append("text")
              .attr("x", 52)
              .attr("y", 59)
              .attr("font-size", "10px")
-              .text("L");
+             .text("L");
 
     if (qtype=='action') {
         var ac = gEl.append("rect")
@@ -687,7 +640,6 @@ function wrapText(gEl, title, numsubs, qtype, perccomplete) {
         // calculate how many words will fit on a line
 
 function calcAllowableWords(maxWidth, words) {
-
     var testLine = "";
     var spacer = "";
     var fittedWidth = 0;
@@ -695,7 +647,6 @@ function calcAllowableWords(maxWidth, words) {
     //ctx.font = font;
 
     for (var i = 0; i < words.length; i++) {
-
         testLine += spacer + words[i];
         spacer = " ";
 
@@ -703,90 +654,60 @@ function calcAllowableWords(maxWidth, words) {
         var width = testLine.length * 5;
         if (width > maxWidth) {
             if (i > 0) {
-            return ({
-                count: i,
-                width: fittedWidth,
-                text: fittedText
-            });}
-            else {return ({
-                count: 1,
-                width: maxWidth,
-                text: words[0]
+            return ({count: i, width: fittedWidth, text: fittedText});}
+            else {return ({count: 1, width: maxWidth, text: words[0]
             });}
         }
 
         fittedWidth = width;
         fittedText = testLine;
-
     }
-    return ({
-                count: i,
-                width: fittedWidth,
-                text: fittedText
-            });
+    return ({count: i, width: fittedWidth, text: fittedText});
 }
 
-function initLines() {
-    var radius  = 80;
-    for (var y = radius * .9; y > -radius; y -= lineHeight) {
-        var h = Math.abs(radius - y);
-        if (y - lineHeight < 0) {
-            h += 20;
-        }
-
-        var length = 2 * Math.sqrt(h * (2 * radius - h)) + 5;
-        if (length && length > 10) {
-            lines.push({
-                y: y,
-                maxLength: length
-            });
+    function initLines() {
+        var radius  = 80;
+        for (var y = radius * .9; y > -radius; y -= lineHeight) {
+            var h = Math.abs(radius - y);
+            if (y - lineHeight < 0) {h += 20;}
+            var length = 2 * Math.sqrt(h * (2 * radius - h)) + 5;
+            if (length && length > 10) {lines.push({y: y, maxLength: length});}
         }
     }
-}
-        function out(m) {
-        $('#target').html(m);
-        }
 
-function addnode(itemtext, posx, posy, qtype) {
-    var nodecolour = "rgb(215,255,215)"
-    if (qtype == 'action') {
-        nodecolour = 'rgb(255,255,220)'
-    }
-    ;
-    if (qtype == 'issue') {
-        nodecolour = 'rgb(215,215,255)'
-    }
-    ;
-    nodes.push({
-        answers: ['yes', 'no'],
-        fillclr: nodecolour,
-        id: nodes.length,
-        locked: "N",
-        priority: 25,
-        qtype: qtype,
-        perccomplete: 0,
-        r: 160,
-        selected: false,
-        fixed: false,
-        scolour: 'orange',
-        linkcount: 0,
-        fontsize: 10,
-        serverid: 0,
-        status: "Draft",
-        swidth: 2,
-        textclr: "white",
-        title: itemtext,
-        xpos: posx,
-        ypos: posy,
-        x: rescale(posx, width, 1000),
-        y: rescale(posy, height, 1000)
-    });
-};
+    function out(m) {$('#target').html(m);}
 
-function updatenode(node, itemtext) {
+    function addnode(itemtext, posx, posy, qtype) {
+        var nodecolour = "rgb(215,255,215)"
+        if (qtype == 'action') {nodecolour = 'rgb(255,255,220)'};
+        if (qtype == 'issue') {nodecolour = 'rgb(215,215,255)'};
+        nodes.push({
+            answers: ['yes', 'no'],
+            fillclr: nodecolour,
+            id: nodes.length,
+            locked: "N",
+            priority: 25,
+            qtype: qtype,
+            perccomplete: 0,
+            r: 160,
+            selected: false,
+            fixed: false,
+            scolour: 'orange',
+            linkcount: 0,
+            fontsize: 10,
+            serverid: 0,
+            status: "Draft",
+            swidth: 2,
+            textclr: "white",
+            title: itemtext,
+            xpos: posx,
+            ypos: posy,
+            x: rescale(posx, width, 1000),
+            y: rescale(posy, height, 1000)});
+    };
+
+    function updatenode(node, itemtext) {
         //console.log(node.serverid, itemtext);
         node.title = itemtext;
         redrawnodes();
-        //console.log('nodes', nodes);
-       redrawnodes();
-}
+    };
