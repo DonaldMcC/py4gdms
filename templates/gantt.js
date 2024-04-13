2ae4d30b73e7198c0adccba825778e3b23a50ebf
Donald <DonaldMcC@users.noreply.github.com>
2024-04-13 12:09:27 +0100

    Add basic jsgantt events

--- templates/gantt.js
+++ templates/gantt.js
@@ -17,6 +17,35 @@ function edit_click() {
 
 g.setShowTaskInfoLink(1);
 g.setEditable(0);
+
+
+g.setOptions({
+// EVENTS
+
+  // OnChangee
+  vEventsChange: {
+    taskname: console.log,
+    res: console.log,
+  },
+  // EventsClickCell
+  vEvents: {
+    taskname: console.log,
+    res: console.log,
+    dur: console.log,
+    comp: console.log,
+    start: console.log,
+    end: console.log,
+    planstart: console.log,
+    planend: console.log,
+    cost: console.log,
+    additional_category: console.log, // for additional fields
+    beforeDraw: ()=>console.log('before draw listener'),
+    afterDraw: ()=>console.log('before after listener')
+  },
+  vEventClickRow: console.log,
+  vEventClickCollapse: console.log
+});
+
 var textstring = "[[=project]]"
 JSGantt.parseXMLString(textstring,g);
 g.Draw();
@@ -34,9 +63,9 @@ function editValue(list, task, event, cell, column) {
   }
 }
 
-/* struggling to find any sort of after edit event - so think we will just have a save button and export
-XML as an option after updating
- */
+/* now found some events and added in with console logging for now */
+/* hopefully new ajax function similar to ones on the actions grid to update */
+/* dates and so forth on change */
 /* https://github.com/jsGanttImproved/jsgantt-improved/pull/61  and demo may help*/
 /*https://github.com/jsGanttImproved/jsgantt-improved/blob/master/docs/index.js#L78 */
 /*https://github.com/jsGanttImproved/jsgantt-improved/blob/master/docs/index.js#L199*/
