711796b5e93bc467c3088b30354d8744c88ac2bf
Donald McClymont <Donaldm2020@gmail.com>
2025-01-03 21:28:43 +0000

    new question updated for simpler structure and queries adjusted - will follow up with calling on the open ai lookup

--- static/js/new_question.js
+++ static/js/new_question.js
@@ -24,23 +24,15 @@ $(document).ready(function(){
    $('#question_execstatus').parent().parent().hide();
    $('#question_xpos').parent().parent().hide();
    $('#question_ypos').parent().parent().hide();
+   $('#question_shared_editing').parent().parent().hide();
+   $('#question_answer3').parent().parent().hide();
+   $('#question_answer4').parent().parent().hide();
    $('#question_correctans').parent().nextAll("p").first().hide();
    $('#question_correctans').hide();
    $('#question_correctanstext').parent().parent().hide();
    $("label[for='question_correctans']").hide();
    //$('#question_aianswer').parent().nextAll("label").first().
     $('#question_aianswer').parent().parent().children().first()
-   $('#question_aianswer').parent().prepend('<div class="btn-group"><input type="BUTTON" id="wolflookup" ' +
-         'value="Wolfram Alpha" class="btn btn-primary" onclick="wolfram_alpha_lookup()">');
-   $('#wolflookup').after('&nbsp&nbsp<input type="BUTTON" id="wikiplookup" ' +
-         'value="Wikipedia" class="btn btn-primary btn-success" onclick="wikipedia_lookup()">');
-   $('#wikiplookup').after('&nbsp&nbsp<input type="BUTTON" id="openailookup" ' +
-         'value="OpenAI" class="btn btn-primary btn-info" onclick="openai_lookup()">');
-  // $('#openailookup').after('&nbsp&nbsp<input type="BUTTON" id="bardlookup" ' +
-   //s      'value="Bard" class="btn btn-primary btn-warning" onclick="bard_lookup()"></div>');
-   $('#openailookup').after('&nbsp&nbsp<input type="BUTTON" id="clearlookup" ' +
-         'value="Clear" class="btn btn-primary btn-danger" onclick="clear_lookup()"></div>');
-
 
    $('#question_aianswer').parent().parent().children().first().append('<div class="btn-group mt-4"><input type="BUTTON" ' +
                 'id="showoptions" value="Show more" class="btn btn-primary" onclick="show_options()">');
@@ -90,41 +82,25 @@ $(document).ready(function(){
                   $('#question_answertext').parent().parent().show();};
                  });
 
+          $('#question_answer2').click(function(){
+                 $('#question_answer3').parent().parent().show();
+              });
+
+          $('#question_answer3').click(function(){
+                 $('#question_answer4').parent().parent().show();
+              });
 
           $('#question_factopinion').change(function(){
-              if($('#question_factopinion option:selected').text()=='Fact')
-                {$('#question_answer1').val('Yes');
-                 $('#question_answer2').val('No');
-                $('#question_answer1').hide();
-                $("label[for='question_answer1']").hide();
-                 $('#question_answer2').hide();
-                 $("label[for='question_answer2']").hide();
-                 $("label[for='question_answertext']").show();
-                 $('#question_answertext').show();
-                 $('#question_status option[value="Resolved"]').prop('selected', true);
-}
+              if($('#question_factopinion option:selected').text()=='Fact') {
+                 $('#question_correctans').parent().nextAll("p").first().show();
+                 $('#question_correctans').show();
+                 $('#question_correctanstext').parent().parent().show();
+                 $("label[for='question_correctans']").show();
+                 $('#question_status option[value="Resolved"]').prop('selected', true);}
               else {
                   $('#question_status option[value="In Progress"]').prop('selected', true);
               }
 
-
-              if($('#question_factopinion option:selected').text()=='Opinion')
-                {$('#question_answer1').show();
-                $("label[for='question_answer1']").show();
-                 $('#question_answer2').show();
-                 $("label[for='question_answer2']").show();
-                 $('#question_answertext').hide();
-                 $("label[for='question_answertext']").hide();}
-
-
-          if($('#question_factopinion option:selected').text()=='AI_Opinion')
-                {$('#question_answer1').show();
-                $("label[for='question_answer1']").show();
-                 $('#question_answer2').show();
-                 $("label[for='question_answer2']").show();
-                 $('#question_answertext').hide();
-                 $("label[for='question_answertext']").hide();
-                 }
                 });
 
                 $('#question_qtype').change();
