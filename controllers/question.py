7947e677e6c6aa3d0e2e1d005164fe43f80588f0
Donald McClymont <Donaldm2020@gmail.com>
2025-04-14 18:42:40 +0100

    fix duplication on grid

--- controllers/question.py
+++ controllers/question.py
@@ -213,6 +213,7 @@ def questiongrid(path=None):
                       ['Search by Event', lambda val: db.event.event_name == val, eventlist],
                       ['Search by Name', lambda val: db.question.questiontext.contains(val)]]
 
+
     if qtype == 'action':
         headings = ['Action', 'Status', 'Execstatus', 'Event', 'Project']
         fields = [db.question.questiontext, db.question.status, db.question.execstatus, db.event.event_name,
@@ -220,27 +221,27 @@ def questiongrid(path=None):
         orderby = [db.question.status, db.question.execstatus, db.question.questiontext]
     elif qtype == 'issue':
         headings = ['Issue', 'Status', 'Event', 'Project']
-        fields = [db.question.questiontext, db.question.status, db.event.event_name,
-                  db.project.proj_name]
+        fields = [db.question.questiontext, db.question.status, db.event.event_name, db.project.proj_name]
         orderby = [db.question.status, db.question.questiontext]
     else:
         headings = ['Question', 'Correct Answer', 'Status', 'Event', 'Project']
         fields = [db.question.questiontext, db.question.correctanstextforgrid, db.question.status, db.event.event_name,
                   db.project.proj_name]
-        fields = [db.question.questiontext, db.question.correctanstextforgrid, db.question.status, db.event.event_name, db.project.proj_name]
+        #fields = [db.question.questiontext, db.question.correctanstextforgrid, db.question.status, db.event.event_name]
         orderby = [db.question.status, db.question.questiontext]
 
     #event_alias = db.event.with_alias('e')
     ##project_alias = db.project.with_alias('p')
 
-    left=[db.event.on(db.question.eventid == db.event.id), db.project.on(db.event.projid == db.project.id)],
+    left= [db.event.on(db.question.eventid == db.event.id), db.project.on(db.event.projid == db.project.id)]
+    #left = db.event.on(db.question.eventid == db.event.id)
     #left = [event_alias.on(db.question.eventid == event_alias.id),project_alias.on(event_alias.projid == project_alias.id)],
 
     grid = Grid(path,
                 queries,
                 fields=fields,
                 headings=headings,
-                left=[db.event.on(db.question.eventid == db.event.id)],
+                left=left,
                 search_queries=search_queries,
                 orderby=orderby,
                 create=URL('new_question/None/' + qtype),
